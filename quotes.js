36d9e9bce1b7d03ced8ddc8b96f7d55d18b475a8
rudranshuojha <rudranshuojha601@gmail.com>
2025-10-28 16:41:37 +0530

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,5 +1,14 @@
 const founderQuotes = [
-
+  "Every great thing starts with late nights and wild ideas"
+  "Innovation is born from obsession"
+  "Code your vision into reality"
+  "Minimal design, maximum purpose"
+  "The only constant in the tech industry is change"
+  "Your job is to build your replacements"
+  "Tech is the useful servant but dangerous master"
+  "AI is just a tool, not a need"
+  "Simplicity is the ultimate sophistication"
+  "A fool with a tool is still a fool"
 ];
 
 function showFounderMessage() {
@@ -8,5 +17,6 @@ founderQuotes[Math.floor(Maths.random()*founderQuotes.length)]
 
 document.getElementById("founder-message").textContent = '"${randomQuote}"';
 }
+
 
 window.onload = showFounderMessage;
